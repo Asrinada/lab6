78e107d0c48efd5771e31a2ed36440ab53e5449a
Asrinada <adakolbasi@gmail.com>
2025-11-19 11:54:43 +0300

    scenario0
    Prints the length, first, third, and last index of a predefined integer array.

---
+++ src/scenario0.java
@@ -0,0 +1,11 @@
+public class scenario0 {
+    public static void main(String[] args) {
+        int[] myArr = {4, 8, 15, 16, 23, 42};
+        System.out.println("length of myArr is " + myArr.length);
+        System.out.println("first elements value is " + myArr[0]);
+        System.out.println("index of the last element is " + (myArr.length - 1));
+        System.out.println("third elements value is " + myArr[2]);
+// arrayin içinde 11 tane değer yok ve biz de arrayin dışına çıkıyoruz
+//System.out.println("10th element of the array is " + myArr[10]);
+    }
+}
